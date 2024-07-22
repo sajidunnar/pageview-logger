e4496db9c94369412bf8a9aa1876f11b1323afe3
Sajid Unar <sajidunnar@gmail.com>
2024-07-23 02:01:39 +0500

    fixing issue of file not found

--- Observer/LogPageView.php
+++ Observer/LogPageView.php
@@ -6,7 +6,7 @@ use Magento\Framework\Event\Observer;
 use Psr\Log\LoggerInterface;
 use Magento\Framework\App\Request\Http;
 use Magento\Framework\View\Page\Title as FrontendTitle;
-use Magento\Backend\Model\View\Result\Page as BackendPage;
+use Magento\Backend\Model\View\Result\PageFactory as BackendPageFactory;
 
 class LogPageView implements ObserverInterface
 {
@@ -14,66 +14,77 @@ class LogPageView implements ObserverInterface
      * @var LoggerInterface
      */
     protected $logger;
+
     /**
      * @var Http
      */
     protected $request;
+
     /**
      * @var FrontendTitle
      */
     protected $frontendTitle;
+
     /**
-     * @var BackendPage
+     * @var BackendPageFactory
      */
-    protected $backendPage;
+    protected $backendPageFactory;
 
     /**
      * @param LoggerInterface $logger
      * @param Http $request
      * @param FrontendTitle $frontendTitle
-     * @param BackendPage $backendPage
+     * @param BackendPageFactory $backendPageFactory
      */
     public function __construct(
         LoggerInterface $logger,
         Http $request,
         FrontendTitle $frontendTitle,
-        BackendPage $backendPage
+        BackendPageFactory $backendPageFactory
     ) {
         $this->logger = $logger;
         $this->request = $request;
         $this->frontendTitle = $frontendTitle;
-        $this->backendPage = $backendPage;
+        $this->backendPageFactory = $backendPageFactory;
     }
 
     /**
+     * Execute method to handle the event
+     *
      * @param Observer $observer
      * @return void
      */
     public function execute(Observer $observer)
     {
-        /**
-         *  get Event from observer and controller action
-         */
+        // Get the controller action from the event
         $action = $observer->getEvent()->getControllerAction();
-        // wrote custom function to get the page title
+        // Get the page title using the custom function
         $pageTitle = $this->getPageTitle();
+        // Get the current page URL
         $pageUrl = $this->request->getUriString();
+        // Get the client IP address
         $clientIp = $this->request->getClientIp();
 
+        // Format the log message
         $logMessage = sprintf('%s: %s - %s', $pageTitle, $pageUrl, $clientIp);
+        // Log the message
         $this->logger->info($logMessage);
     }
 
     /**
-     * @return mixed
+     * Get the page title based on the request area (frontend or admin)
+     *
+     * @return string
      */
     protected function getPageTitle()
     {
         if ($this->request->getRouteName() === 'adminhtml') {
-            $pageTitle = $this->backendPage->getConfig()->getTitle()->get();
+            // Create a backend page object and get the title
+            $backendPage = $this->backendPageFactory->create();
+            return $backendPage->getConfig()->getTitle()->get();
         } else {
-            $pageTitle = $this->frontendTitle->get();
+            // Get the frontend page title
+            return $this->frontendTitle->get();
         }
-        return $pageTitle;
     }
 }
